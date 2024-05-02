938bd47e13492e652feed67e9e09044e1a6f5f02
rabinabaga <rabinabaga@gmail.com>
2024-05-02 14:02:12 +0545

    Revert "socket.io configuration done"
    This reverts commit 98f0b004c5ecd68fefb67048c8df301964191db4.

--- instagram-fcc/backend/index.js
+++ instagram-fcc/backend/index.js
@@ -1,25 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
 const app = require("./src/config/express.config.js");
-const http = require('http').Server(app);
-const socketIO = require('socket.io')(http,{cors:{origin: "*   " }});
 
-
-
-//event connection of a client from the frontend
-socketIO.on('connection', (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
-  socket.on('disconnect', () => {
-    console.log('🔥: A user disconnected');
-  });
-  socket.on('message', (data) => {
-    console.log(data);
-    socketIO.emit('messageResponse', data);
-  });
-// io.emit('message', `${socket.id.substr(0,2)} said ${message}`)
-}
-);
-const server_new = http.listen(8001, "0.0.0.0", (err) => {
+const http = require("http");
+const server = http.createServer(app);
+const server_new = server.listen(8001, "0.0.0.0", (err) => {
   if (!err) {
     console.log("server is running on port", process.env.PORT);
   }
