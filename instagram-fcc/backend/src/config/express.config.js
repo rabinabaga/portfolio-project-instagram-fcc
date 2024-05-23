ecb69ecbf6a9696fb2f48a357b5670b575c58763
rabinabaga <rabinabaga@gmail.com>
2024-05-23 17:09:18 +0545

    setting up validation in backend with zod

--- instagram-fcc/backend/src/config/express.config.js
+++ instagram-fcc/backend/src/config/express.config.js
@@ -1,29 +1,40 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { ZodError } = require("zod");
 
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 require("./mongodb.config");
-
 
 const routes = require("../routes");
 app.use("/api/v1", routes);
 
 app.use((req, res, next) => {
-    next({ code: 404, message: "Not found" });
-  });
-  
-  app.use((error, req, res, next) => {
-    let code = error.code ?? 500;
-    let msg = "external server error";
-  
-    res.status(code).json({
-      result: null,
-      msg: msg,
-      meta: null,
+  next({ code: 404, message: "Not found" });
+});
+
+app.use((error, req, res, next) => {
+  let data = {};
+  let errBag;
+  let code;
+
+  if (error instanceof ZodError) {
+    error.code = 403;
+    errBag = error.errors.map((error) => {
+      return { ...data, [error["path"][0]]: error.message };
     });
+  }
+
+  code = error.code ?? 500;
+  let msg = errBag ?? "external server error";
+
+  res.status(code).json({
+    result: null,
+    msg: msg,
+    meta: null,
   });
+});
 module.exports = app;
