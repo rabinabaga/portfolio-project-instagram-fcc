c39f7ebe9630458c842f917c5f54a3addc633e30
rabinabaga <rabinabaga@gmail.com>
2024-05-23 23:35:26 +0545

    added string email must have at least 50 characters to distinguish frontend validation and backend one

--- instagram-fcc/backend/src/app/auth/auth.validator.js
+++ instagram-fcc/backend/src/app/auth/auth.validator.js
@@ -1,7 +1,7 @@
 const { z } = require("zod");
 
 const loginSchema = z.object({
-    email: z.string().email().min(10),
+    email: z.string().email().min(50),
     password: z.string().min(6),
   });
 
