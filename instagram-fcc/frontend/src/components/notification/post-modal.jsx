6fb799f66ff123ad8e76c36ef9f49a1681815340
rabinabaga <rabinabaga@gmail.com>
2024-06-02 22:39:27 +0545

    modal while displaying post from notification made beautiful

--- instagram-fcc/frontend/src/components/notification/post-modal.jsx
+++ instagram-fcc/frontend/src/components/notification/post-modal.jsx
@@ -2,29 +2,17 @@ import Post from "../Post";
 
 function PostModal({ handleClick, content }) {
   return (
-    <>
-      <div
-        id="authentication-modal"
-        tabindex="-1"
-        aria-hidden="true"
-        className={`${
-          Object.keys(content).length > 0 ? "block" : "hidden"
-        } border border-2 border-black `}
-      >
-        <div className="relative w-full border border-black border-8 border-slate-500">
-          <div className=" fixed left-96 w-96 h-48 overflow-y  mx-auto p-4 shadow-cyan-500/50 rounded-lg shadow dark:bg-gray-700">
-            <div
-              className="text-4xl absolute w-10 h-10 top-0 right-0 rounded-full bg-indigo-500 text-white cursor-pointer"
-              onClick={handleClick}
-            >
-              &#215;
-            </div>
-
-            <Post content={content} />
-          </div>
+    <div
+      className={`${
+        Object.keys(content).length > 0 ? "block" : "hidden"
+      } fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50`}
+    >
+      <div className="bg-white rounded-lg p-8 w-11/12 md:max-w-md mx-auto">
+        <div className="h-96 overflow-y-auto">
+          <Post content={content}></Post>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
