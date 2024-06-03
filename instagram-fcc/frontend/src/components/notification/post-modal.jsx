c5ba7dd0bf1a94ca37bb86274cede9cbb9816b63
rabinabaga <rabinabaga@gmail.com>
2024-06-03 07:43:54 +0545

    completed post modal in profile

--- instagram-fcc/frontend/src/components/notification/post-modal.jsx
+++ instagram-fcc/frontend/src/components/notification/post-modal.jsx
@@ -7,8 +7,14 @@ function PostModal({ handleClick, content }) {
         Object.keys(content).length > 0 ? "block" : "hidden"
       } fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50`}
     >
-      <div className="bg-white rounded-lg p-8 w-11/12 md:max-w-md mx-auto">
+      <div className="relative bg-white rounded-lg p-8 w-11/12 md:max-w-md mx-auto">
         <div className="h-96 overflow-y-auto">
+          <div
+            className="text-4xl absolute w-10 h-10 top-0 right-0 rounded-full bg-indigo-500 text-white cursor-pointer"
+            onClick={handleClick}
+          >
+            &#215;
+          </div>
           <Post content={content}></Post>
         </div>
       </div>
